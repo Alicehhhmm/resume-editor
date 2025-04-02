7bbb6e3441f7850045ed15fc309d5c1a0f1588b1
Alicehhhmm <a007aplum@gmail.com>
2025-04-02 19:15:11 +0800

    chore: update layout file

---
+++ components/provider/theme-providers.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import * as React from 'react'
+
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+
+export function RThemeProvider({
+    children,
+    ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
+    return (
+        <NextThemesProvider
+            attribute="data-theme"
+            defaultTheme="light"
+            enableSystem
+            storageKey="resume-theme"
+            {...props}
+        >
+            {children}
+        </NextThemesProvider>
+    )
+}
