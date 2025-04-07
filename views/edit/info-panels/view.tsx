6369bec1c0faf7555b667f78d06fb33be6690070
Alicehhhmm <a007aplum@gmail.com>
2025-04-07 15:34:15 +0800

    fix:  add  info-panels

---
+++ views/edit/info-panels/view.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { ColorPicker } from '@/components/editor/color-picker'
+
+import { useCanvas } from '@/hooks/use-canvas'
+
+export const RightPanel = () => {
+    const canvas = useCanvas()
+    const { currentTemplate } = canvas
+
+    return (
+        <div>
+            <h1>RightPanel</h1>
+
+            {/* base info */}
+            <div className="">{currentTemplate.title}</div>
+
+            {/* color row */}
+            <ColorPicker value="" onChange={() => {}} />
+
+            {/* font row */}
+            <div className="">字体配置</div>
+
+            {/* layout row*/}
+            <div className="">布局间隙</div>
+        </div>
+    )
+}
